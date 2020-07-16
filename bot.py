a50ea626f072f8e77cd46a64f1ad71535a0d2b2d
TahaRangwala <tahamr@vt.edu>
2020-07-16 15:30:46 -0400

    adding commands

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import random
 
 client = commands.Bot(command_prefix = '.')
 
@@ -13,5 +14,45 @@ token = read_token()
 @client.event
 async def on_ready():
     print('Bot is ready.')
+
+@client.event
+async def on_member_join(member):
+    print(f'{member} has joined a server.')
+
+@client.event
+async def on_member_remove(member):
+    print(f'{member} left the server.')
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'My ping is {round(client.latency * 1000)}ms')
+
+@client.command(aliases = ['8ball'])
+async def _8ball(ctx,*, question):
+    responses = ["It is certain.",
+                "It is decidedly so.",
+                "Without a doubt.",
+                "Yes - definitely.",
+                "You may rely on it.",
+                "As I see it, yes.",
+                "Most likely.",
+                "Outlook good.",
+                "Yes.",
+                "Signs point to yes.",
+                "Reply hazy, try again.",
+                "Ask again later.",
+                "Better not tell you now.",
+                "Cannot predict now.",
+                "Concentrate and ask again.",
+                "Don't count on it.",
+                "My reply is no.",
+                "My sources say no.",
+                "Outlook not so good.",
+                "Very doubtful."]
+    await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
+
+@client.command
+async def clear(ctx, amount = 1):
+    await ctx.channel.purge(limit = amount)
 
 client.run(token)
