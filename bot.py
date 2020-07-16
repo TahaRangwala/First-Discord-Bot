2bca2a1e94e9160cfa4cf8b67f1865e591b27191
TahaRangwala <tahamr@vt.edu>
2020-07-16 13:36:29 -0400

    bot stuff

--- bot.py
+++ bot.py
@@ -1,2 +1,17 @@
 import discord
+from discord.ext import commands
 
+client = commands.Bot(command_prefix = '.')
+
+def read_token():
+    with open("token.txt", "r") as f:
+        lines = f.readlines()
+        return lines[0].strip()
+
+token = read_token()
+
+@client.event
+async def on_ready():
+    print('Bot is ready.')
+
+client.run(token)
